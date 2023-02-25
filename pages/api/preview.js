5e272d6b252c7b25d10d2c25c73fc172772cb4eb
RIUCI Admin <riuciadmin@inbar.int>
2023-02-25 20:32:48 -0300

    Social Share Slice Deleted

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,11 +1,8 @@
 import * as prismicNext from "@prismicio/next";
-
 import { createClient, linkResolver } from "prismicio";
 
 export default async function handler(req, res) {
   const client = createClient({ req });
-
   prismicNext.setPreviewData({ req, res });
-
   await prismicNext.redirectToPreviewURL({ req, res, client, linkResolver });
 }
