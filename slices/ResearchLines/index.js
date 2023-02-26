edc0ab5e4b1fb46781848cbbd145bef7cd5a8e44
RIUCI Admin <riuciadmin@inbar.int>
2023-02-26 13:04:50 -0300

    Research Lines Cards

--- slices/ResearchLines/index.js
+++ slices/ResearchLines/index.js
@@ -11,15 +11,18 @@ const ResearchLines = ({ slice }) => (
       <h2 className="text-center md:pb-3">{slice.primary.title}</h2>
       <div className="grid grid-cols-1 gap-5 pt-5 md:grid-cols-2 md:gap-11 lg:grid-cols-3">
         {slice.items.map((item, i) => (
-          <div className="grid grid-cols-12" key={i}>
-            <span className="col-span-2 text-3xl text-green-dark">
+          <div
+            className="flex h-40 flex-col items-center rounded-md border-[1px] border-neutral-300 p-5 text-center shadow-md"
+            key={i}
+          >
+            <div className="pb-3 text-3xl text-green-dark">
               {item.icon === "bamboo" ? (
                 <BambooSVG />
               ) : (
                 <i className={`fa-solid fa-${item.icon} mt-1`}></i>
               )}
-            </span>
-            <span className="col-span-10">{item.topic}</span>
+            </div>
+            <div>{item.topic}</div>
           </div>
         ))}
       </div>
