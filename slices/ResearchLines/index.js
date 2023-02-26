e9bf6fb55bdbab3b23d8e0b2d27c5cbf6c7a13c1
RIUCI Admin <riuciadmin@inbar.int>
2023-02-26 13:15:04 -0300

    Research Lines Font

--- slices/ResearchLines/index.js
+++ slices/ResearchLines/index.js
@@ -12,7 +12,7 @@ const ResearchLines = ({ slice }) => (
       <div className="grid grid-cols-1 gap-5 pt-5 md:grid-cols-2 md:gap-11 lg:grid-cols-3">
         {slice.items.map((item, i) => (
           <div
-            className="flex h-40 flex-col items-center rounded-md border-[1px] border-neutral-300 p-5 text-center shadow-md"
+            className="flex h-40 flex-col items-center justify-center rounded-md border-[1px] border-neutral-300 p-5 text-center shadow-md md:justify-start"
             key={i}
           >
             <div className="pb-3 text-3xl text-green-dark">
@@ -22,7 +22,7 @@ const ResearchLines = ({ slice }) => (
                 <i className={`fa-solid fa-${item.icon} mt-1`}></i>
               )}
             </div>
-            <div>{item.topic}</div>
+            <div className="font-medium">{item.topic}</div>
           </div>
         ))}
       </div>
