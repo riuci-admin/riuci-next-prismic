34ad18cd93d9c071ac1dfeb63e4d8bc13149b4b5
RIUCI Admin <riuciadmin@inbar.int>
2023-03-03 13:16:26 -0300

    Nabvar Search Icon

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { PrismicNextImage } from "@prismicio/next";
 import { PrismicLink } from "@prismicio/react";
+import Link from "next/link";
 import { linkResolver } from "prismicio";
 
 const FlagIcon = ({ lang }) => {
@@ -66,6 +67,24 @@ export const Header = ({ alternateLanguages = [], navigation, settings }) => {
                   </li>
                 );
               })}
+              <li className="flex items-center text-green-dark">
+                <Link href="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </Link>
+              </li>
               {alternateLanguages.map((lang) => (
                 <li className="flex items-center" key={lang.lang}>
                   <PrismicLink href={linkResolver(lang)} locale={lang.lang}>
