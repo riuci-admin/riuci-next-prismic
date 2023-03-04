a45fc722595f0eb768da444b2a1c64e20bc6c12d
RIUCI Admin <riuciadmin@inbar.int>
2023-03-04 00:14:26 -0300

    Research Lines Cards

--- slices/ResearchLines/index.js
+++ slices/ResearchLines/index.js
@@ -6,25 +6,24 @@ import React from "react";
  * @param { ResearchLinesProps }
  */
 const ResearchLines = ({ slice }) => (
-  <section className="py">
+  <section className="py bg-neutral-50">
     <div className="frame pb-7">
       <h2 className="text-center text-green-dark md:pb-3">
         {slice.primary.title}
       </h2>
       <div className="grid grid-cols-1 gap-5 pt-5 md:grid-cols-2 md:gap-11 lg:grid-cols-3">
         {slice.items.map((item, i) => (
-          <div
-            className="flex h-[11.5rem] flex-col items-center justify-center rounded-md border-[1px] border-neutral-300 p-5 text-center shadow-md md:justify-start"
-            key={i}
-          >
-            <div className="pb-3 text-5xl text-green-dark opacity-80">
-              {item.icon === "bamboo" ? (
-                <BambooSVG />
-              ) : (
-                <i className={`fa-solid fa-${item.icon} mt-1`}></i>
-              )}
+          <div className="card bg-white shadow-xl" key={i}>
+            <div className="card-body flex items-center text-center">
+              <div className="pb-3 text-5xl text-green-dark opacity-80">
+                {item.icon === "bamboo" ? (
+                  <BambooSVG />
+                ) : (
+                  <i className={`fa-solid fa-${item.icon} mt-1`}></i>
+                )}
+              </div>
+              <div className="font-medium">{item.topic}</div>
             </div>
-            <div className="font-medium">{item.topic}</div>
           </div>
         ))}
       </div>
