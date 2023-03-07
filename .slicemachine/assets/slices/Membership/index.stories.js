bc20f230e15eff72914e83970b919622adacd4f8
RIUCI Admin <riuciadmin@inbar.int>
2023-03-07 11:01:48 -0300

    Membership Link Allow Blank

--- .slicemachine/assets/slices/Membership/index.stories.js
+++ .slicemachine/assets/slices/Membership/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":"model","description":[{"type":"paragraph","text":"Sit enim eu ipsum adipisicing nostrud adipisicing eu in et aliqua.","spans":[]}]},"id":"_Default","slice_type":"membership"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":"model","description":[{"type":"paragraph","text":"Exercitation velit fugiat dolore ea dolor Lorem.","spans":[]}]},"id":"_Default","slice_type":"membership"}} />
 _Default.storyName = ''
