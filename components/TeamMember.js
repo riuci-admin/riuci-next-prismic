cb3501f6bd83fc26184917113aac232127db4f07
RIUCI Admin <riuciadmin@inbar.int>
2023-03-17 20:01:12 -0300

    Team Member Top Margin

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -2,7 +2,7 @@ import { PrismicNextImage } from "@prismicio/next";
 
 export const TeamMember = ({ photo, member, title }) => (
   <div className="card max-w-[19rem] border-[1px] border-neutral-100 bg-white shadow-xl">
-    <div className="avatar mt-5 flex justify-center">
+    <div className="avatar mt-6 flex justify-center">
       <div className="w-36 rounded-full">
         <PrismicNextImage
           field={photo}
