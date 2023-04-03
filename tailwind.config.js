77ae7e779102f99f1b1a822d587a6b01e12a2118
RIUCI Admin <riuciadmin@inbar.int>
2023-04-03 16:52:42 -0500

    Hero Background Small Screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         inter: "var(--font-inter)",
       },
+      screens: {
+        xs: "421px",
+      },
       colors: {
         "green-light": "#F7FFF0",
         "green-soft": "#A8CA45",
