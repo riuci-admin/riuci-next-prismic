fb412a5f5c8385f49e76d66d598b8dc6cf2a4f2e
RIUCI Admin <riuciadmin@inbar.int>
2023-04-19 15:05:12 -0500

    Research Lines Flex Layout

--- slices/ResearchLines/index.js
+++ slices/ResearchLines/index.js
@@ -9,10 +9,10 @@ const ResearchLines = ({ slice }) => (
   <section className="bg-neutral-100">
     <div className="frame py">
       <h2>{slice.primary.title}</h2>
-      <div className="grid grid-cols-1 gap-11 pt-3 md:grid-cols-2 md:gap-14 lg:grid-cols-3">
+      <div className="flex flex-wrap justify-center gap-11 pt-3 md:gap-14">
         {slice.items.map((item, i) => (
-          <div className="card h-52 bg-white shadow-xl" key={i}>
-            <div className="card-body flex items-center text-center">
+          <div key={i} className="card h-52 w-80 bg-white shadow-xl">
+            <div className="card-body flex items-center justify-center text-center md:justify-start">
               <div className="pb-3 text-5xl text-green-dark opacity-80">
                 {item.icon === "bamboo" ? (
                   <BambooSVG />
