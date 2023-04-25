f0a28f28a8205697c6ffe9a41789cdee4c72b31f
RIUCI Admin <riuciadmin@inbar.int>
2023-04-25 14:25:19 -0500

    News Image Optimization

--- components/RichTextComponents.js
+++ components/RichTextComponents.js
@@ -2,24 +2,14 @@ import { PrismicNextImage } from "@prismicio/next";
 import { PrismicLink } from "@prismicio/react";
 
 export const richTextComponents = {
-  paragraph: ({ children }) => (
-    <p className="mb-3 leading-normal last:mb-0">{children}</p>
-  ),
-  oList: ({ children }) => (
-    <ol className="mb-7 pl-4 last:mb-0 md:pl-6">{children}</ol>
-  ),
+  paragraph: ({ children }) => <p className="mb-3 leading-normal last:mb-0">{children}</p>,
+  oList: ({ children }) => <ol className="mb-7 pl-4 last:mb-0 md:pl-6">{children}</ol>,
   oListItem: ({ children }) => (
     <li className="mb-1 list-decimal pl-1 last:mb-0 md:pl-2">{children}</li>
   ),
-  list: ({ children }) => (
-    <ul className="mb-7 pl-4 last:mb-0 md:pl-6">{children}</ul>
-  ),
-  listItem: ({ children }) => (
-    <li className="mb-1 list-disc pl-1 last:mb-0 md:pl-2">{children}</li>
-  ),
-  strong: ({ children }) => (
-    <strong className="font-semibold">{children}</strong>
-  ),
+  list: ({ children }) => <ul className="mb-7 pl-4 last:mb-0 md:pl-6">{children}</ul>,
+  listItem: ({ children }) => <li className="mb-1 list-disc pl-1 last:mb-0 md:pl-2">{children}</li>,
+  strong: ({ children }) => <strong className="font-semibold">{children}</strong>,
   hyperlink: ({ children, node }) => (
     <PrismicLink field={node.data} className="link">
       {children}
@@ -27,7 +17,7 @@ export const richTextComponents = {
   ),
   image: ({ node }) => (
     <div className="pb-3">
-      <PrismicNextImage field={node} />
+      <PrismicNextImage field={node} priority />
     </div>
   ),
 };
