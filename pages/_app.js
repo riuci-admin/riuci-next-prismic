cb5426013748260555ce0fd110a60467d65c4289
RIUCI Admin <riuciadmin@inbar.int>
2023-04-26 13:46:57 -0500

    Vercel Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "styles/globals.css";
 
 import { PrismicProvider } from "@prismicio/react";
+import { Analytics } from "@vercel/analytics/react";
 import { richTextComponents } from "components/RichTextComponents";
 import Link from "next/link";
-import Script from "next/script";
 import { linkResolver } from "prismicio";
 
 export default function App({ Component, pageProps }) {
@@ -13,20 +13,8 @@ export default function App({ Component, pageProps }) {
       linkResolver={linkResolver}
       richTextComponents={richTextComponents}
     >
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-6V3WZD4B9L"
-        strategy="lazyOnload"
-      />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-6V3WZD4B9L');
-        `}
-      </Script>
       <Component {...pageProps} />
+      <Analytics />
     </PrismicProvider>
   );
 }
